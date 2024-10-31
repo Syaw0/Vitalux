636763806667b1684719c44e13aba41ea771bde9
syaw0 <siamhb7@protonmail.com>
2024-10-31 10:06:42 +0330

    feat: Add ANSIEscapeCode struct with accompanying tests
    this struct like a placeholder to hold a ansi code and act like abstraction

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+struct ANSIEscapeCode {
+    parameter: String,
+}
+impl ANSIEscapeCode {
+    fn new(parameter: String) -> Self {
+        ANSIEscapeCode { parameter }
+    }
+
+    fn code(&self) -> String {
+        format!("\\x1b[{}m", self.parameter)
+    }
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn create_simple_ansi_code() {
+        let p = ANSIEscapeCode::new(String::from("33"));
+        assert_eq!(p.code(), "\\x1b[33m")
+    }
+
+    #[test]
+    fn create_reset_ansi_code() {
+        let reset_ansi = ANSIEscapeCode::new(String::from("0"));
+        assert_eq!("\\x1b[0m", reset_ansi.code());
     }
 }
