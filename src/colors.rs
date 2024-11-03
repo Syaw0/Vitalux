ca0f229412d7c31cafd1b0e65d2e833c4f5a937d
syaw0 <siamhb7@protonmail.com>
2024-11-03 09:43:07 +0330

    feat: Add Style enum and store style variants with their structs
    In this way we can easily get inside each variant and check the concrete type, that could not be done with traint objects and dyn trait

--- src/colors.rs
+++ src/colors.rs
@@ -1,9 +1,28 @@
 pub trait Stylify {
     fn make_styles(&self, paint_type: Option<PaintType>) -> String;
 }
+
 pub enum PaintType {
     FG,
     BG,
+}
+
+pub enum Styles {
+    StyleRgb(Rgb),
+    StyleBasicColor(BasicColor),
+    StylePaletteColor(PaletteColor),
+    StylePaintType(PaintType),
+}
+
+impl Styles {
+    fn make_styles(&self, paint_type: Option<PaintType>) -> String {
+        match self {
+            Styles::StyleBasicColor(c) => c.make_styles(paint_type),
+            Styles::StylePaintType(c) => c.make_styles(paint_type),
+            Styles::StylePaletteColor(c) => c.make_styles(paint_type),
+            Styles::StyleRgb(c) => c.make_styles(paint_type),
+        }
+    }
 }
 
 impl Stylify for PaintType {
@@ -28,22 +47,22 @@ impl Stylify for BasicColor {
     }
 }
 
-pub const BLACK: BasicColor = BasicColor { fg: 30, bg: 40 };
-pub const RED: BasicColor = BasicColor { fg: 31, bg: 41 };
-pub const GREEN: BasicColor = BasicColor { fg: 32, bg: 42 };
-pub const YELLOW: BasicColor = BasicColor { fg: 33, bg: 43 };
-pub const BLUE: BasicColor = BasicColor { fg: 34, bg: 44 };
-pub const MAGENTA: BasicColor = BasicColor { fg: 35, bg: 45 };
-pub const CYAN: BasicColor = BasicColor { fg: 36, bg: 46 };
-pub const WHITE: BasicColor = BasicColor { fg: 37, bg: 47 };
-pub const GRAY: BasicColor = BasicColor { fg: 90, bg: 10 };
-pub const BRIGHT_RED: BasicColor = BasicColor { fg: 91, bg: 101 };
-pub const BRIGHT_GREEN: BasicColor = BasicColor { fg: 92, bg: 102 };
-pub const BRIGHT_YELLOW: BasicColor = BasicColor { fg: 93, bg: 103 };
-pub const BRIGHT_BLUE: BasicColor = BasicColor { fg: 94, bg: 104 };
-pub const BRIGHT_MAGENTA: BasicColor = BasicColor { fg: 95, bg: 105 };
-pub const BRIGHT_CYAN: BasicColor = BasicColor { fg: 96, bg: 106 };
-pub const BRIGHT_WHITE: BasicColor = BasicColor { fg: 97, bg: 107 };
+pub const BLACK: Styles = Styles::StyleBasicColor(BasicColor { fg: 30, bg: 40 });
+pub const RED: Styles = Styles::StyleBasicColor(BasicColor { fg: 31, bg: 41 });
+pub const GREEN: Styles = Styles::StyleBasicColor(BasicColor { fg: 32, bg: 42 });
+pub const YELLOW: Styles = Styles::StyleBasicColor(BasicColor { fg: 33, bg: 43 });
+pub const BLUE: Styles = Styles::StyleBasicColor(BasicColor { fg: 34, bg: 44 });
+pub const MAGENTA: Styles = Styles::StyleBasicColor(BasicColor { fg: 35, bg: 45 });
+pub const CYAN: Styles = Styles::StyleBasicColor(BasicColor { fg: 36, bg: 46 });
+pub const WHITE: Styles = Styles::StyleBasicColor(BasicColor { fg: 37, bg: 47 });
+pub const GRAY: Styles = Styles::StyleBasicColor(BasicColor { fg: 90, bg: 10 });
+pub const BRIGHT_RED: Styles = Styles::StyleBasicColor(BasicColor { fg: 91, bg: 101 });
+pub const BRIGHT_GREEN: Styles = Styles::StyleBasicColor(BasicColor { fg: 92, bg: 102 });
+pub const BRIGHT_YELLOW: Styles = Styles::StyleBasicColor(BasicColor { fg: 93, bg: 103 });
+pub const BRIGHT_BLUE: Styles = Styles::StyleBasicColor(BasicColor { fg: 94, bg: 104 });
+pub const BRIGHT_MAGENTA: Styles = Styles::StyleBasicColor(BasicColor { fg: 95, bg: 105 });
+pub const BRIGHT_CYAN: Styles = Styles::StyleBasicColor(BasicColor { fg: 96, bg: 106 });
+pub const BRIGHT_WHITE: Styles = Styles::StyleBasicColor(BasicColor { fg: 97, bg: 107 });
 
 //
 
@@ -65,13 +84,13 @@ impl Stylify for PaletteColor {
     }
 }
 
-pub struct RGB {
+pub struct Rgb {
     pub r: u8,
     pub g: u8,
     pub b: u8,
 }
 
-impl Stylify for RGB {
+impl Stylify for Rgb {
     fn make_styles(&self, paint_type: Option<PaintType>) -> String {
         let paint_type = paint_type.unwrap_or(PaintType::FG);
         format!(
@@ -95,14 +114,16 @@ mod test {
 
     #[test]
     fn black_colors() {
-        assert_eq!(30, BLACK.fg);
-        assert_eq!(40, BLACK.bg)
+        let black = BasicColor { fg: 30, bg: 40 };
+        assert_eq!(30, black.fg);
+        assert_eq!(40, black.bg)
     }
 
     #[test]
     fn cyan_colors() {
-        assert_eq!(36, CYAN.fg);
-        assert_eq!(46, CYAN.bg)
+        let cyan = BasicColor { fg: 36, bg: 46 };
+        assert_eq!(36, cyan.fg);
+        assert_eq!(46, cyan.bg)
     }
 
     #[test]
@@ -147,7 +168,7 @@ mod test {
 
     #[test]
     fn red_rgb_color() {
-        let red = RGB { r: 255, g: 0, b: 0 };
+        let red = Rgb { r: 255, g: 0, b: 0 };
         assert_eq!(255, red.r);
         assert_eq!(0, red.g);
         assert_eq!(0, red.b)
@@ -155,7 +176,7 @@ mod test {
 
     #[test]
     fn salmon_rgb_color() {
-        let salmon = RGB { r: 250, g: 128, b: 114 };
+        let salmon = Rgb { r: 250, g: 128, b: 114 };
         assert_eq!(250, salmon.r);
         assert_eq!(128, salmon.g);
         assert_eq!(114, salmon.b)
@@ -163,14 +184,14 @@ mod test {
 
     #[test]
     fn rgb_paint_purple_fg() {
-        let purple = RGB { r: 126, g: 50, b: 219 };
+        let purple = Rgb { r: 126, g: 50, b: 219 };
         let painted_fg = purple.make_styles(None);
         assert_eq!("38;2;126;50;219", painted_fg)
     }
 
     #[test]
     fn rgb_paint_orange_bg() {
-        let orange = RGB { r: 219, g: 132, b: 50 };
+        let orange = Rgb { r: 219, g: 132, b: 50 };
         let painted_bg = orange.make_styles(Some(PaintType::BG));
         assert_eq!("48;2;219;132;50", painted_bg)
     }
