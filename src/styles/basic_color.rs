4b4126e89bc7078af0f6970950cf34d07ca0b7b2
syaw0 <siamhb7@protonmail.com>
2024-11-06 10:37:36 +0330

    test(basic_color.rs): Write test for coverage all of the module

--- src/styles/basic_color.rs
+++ src/styles/basic_color.rs
@@ -47,18 +47,87 @@ mod tests {
     use super::*;
 
     #[test]
-    fn black_color() {
-        match BLACK {
-            Styles::StyleBasicColor(BasicColor { fg: 30, bg: 40 }) => assert!(true),
-            _ => panic!("It's not a `Black` color! black color should have fg:30 and bg:40"),
-        }
+    fn test_make_style_fg() {
+        let color = BasicColor { fg: 30, bg: 40 };
+        let styles = color.make_styles(Some(&PaintType::FG));
+        assert_eq!(styles, "30")
     }
 
     #[test]
-    fn red_color() {
-        match RED {
-            Styles::StyleBasicColor(BasicColor { fg: 31, bg: 41 }) => assert!(true),
-            _ => panic!("It's not a `Red` color! red color should have fg:31 and bg:41"),
+    fn test_make_style_default_fg() {
+        let color = BasicColor { fg: 34, bg: 44 };
+        let styles = color.make_styles(None);
+        assert_eq!(styles, "34")
+    }
+
+    #[test]
+    fn test_make_style_bg() {
+        let color = BasicColor { fg: 30, bg: 40 };
+        let styles = color.make_styles(Some(&PaintType::BG));
+        assert_eq!(styles, "40")
+    }
+
+    #[test]
+    fn test_fg_and_bg_values() {
+        let color = BasicColor { fg: 30, bg: 40 };
+        let styles_fg = color.make_styles(Some(&PaintType::FG));
+        let styles_bg = color.make_styles(Some(&PaintType::BG));
+        assert_eq!(styles_fg, "30");
+        assert_eq!(styles_bg, "40");
+    }
+
+    #[test]
+    fn test_fg_and_bg_values_with_none_paint_type() {
+        let color = BasicColor { fg: 30, bg: 40 };
+        let styles_fg = color.make_styles(None);
+        let styles_bg = color.make_styles(Some(&PaintType::BG));
+        assert_eq!(styles_fg, "30");
+        assert_eq!(styles_bg, "40");
+    }
+
+    #[test]
+    fn test_color_code_macro() {
+        color_code!(TEST_COLOR,{fg:100,bg:200});
+        match TEST_COLOR {
+            Styles::StyleBasicColor(BasicColor { fg, bg }) => {
+                assert_eq!(fg, 100);
+                assert_eq!(bg, 200);
+            }
+            _ => panic!("TEST_COLOR is not a BasicColor"),
         }
     }
+
+    macro_rules! color_test {
+        ($test_name:ident, $color_name:ident, $color:expr, $fg:expr, $bg:expr) => {
+            #[test]
+            fn $test_name(){
+                match $color_name{
+                    Styles::StyleBasicColor(BasicColor{fg,bg})=>{
+                        assert_eq!(fg,$fg);
+                        assert_eq!(bg,$bg);
+                    },
+                    _=>{
+                        panic!("This color is not a basic color!"); 
+                    }
+                }
+            }
+        };
+    }
+
+    color_test!(black_color, BLACK, "Black", 30, 40);
+    color_test!(red_color, RED, "Red", 31, 41);
+    color_test!(green_color, GREEN, "Green", 32, 42);
+    color_test!(yellow_color, YELLOW, "Yellow", 33, 43);
+    color_test!(blue_color, BLUE, "Blue", 34, 44);
+    color_test!(magenta_color, MAGENTA, "Magenta", 35, 45);
+    color_test!(cyan_color, CYAN, "Cyan", 36, 46);
+    color_test!(white_color, WHITE, "White", 37, 47);
+    color_test!(gray_color, GRAY, "Gray", 90, 100);
+    color_test!(bright_red_color, BRIGHT_RED, "Bright Red", 91, 101);
+    color_test!(bright_green_color, BRIGHT_GREEN, "Bright Green", 92, 102);
+    color_test!(bright_yellow_color, BRIGHT_YELLOW, "Bright Yellow", 93, 103);
+    color_test!(bright_blue_color, BRIGHT_BLUE, "Bright Blue", 94, 104);
+    color_test!(bright_magenta_color, BRIGHT_MAGENTA, "Bright Magenta", 95, 105);
+    color_test!(bright_cyan_color, BRIGHT_CYAN, "Bright Cyan", 96, 106);
+    color_test!(bright_white_color, BRIGHT_WHITE, "Bright White", 97, 107);
 }
