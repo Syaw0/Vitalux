1302bcd53ed67ecd2f14c03d726465d587d636fc
syaw0 <siamhb7@protonmail.com>
2024-11-07 10:44:31 +0330

    doc(ansi_code.rs): add basic document for a module

--- src/ansi_code.rs
+++ src/ansi_code.rs
@@ -1,11 +1,25 @@
+/// A module for creating ansi escape code.
+///
+/// This module provides an struct `ANSIEscapeCode`
+/// that represents ansi escape code, with `parameter` field.
+/// It also implements the `ANSIEscapeCode`, with `new` and `code` method
+/// which allows for generating anis escape code and getting a code.
+
+// =======================================================================
+
+/// An struct representing ansi escape code.
+///
+/// This struct has 1 field: `parameter` , which represent String of the ansi code.
 pub struct ANSIEscapeCode {
     parameter: String,
 }
 impl ANSIEscapeCode {
+    /// Returns a ANSIEscapeCode instance with parameter .
     pub fn new(parameter: &str) -> Self {
         ANSIEscapeCode { parameter: parameter.to_string() }
     }
 
+    /// Returns a String that represent the ansi code.
     pub fn code(&self) -> String {
         format!("\x1b[{}m", self.parameter)
     }
