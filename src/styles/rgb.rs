745d58294d6d3b5159e76e54863bdfb9d3d83388
syaw0 <siamhb7@protonmail.com>
2024-11-07 10:37:13 +0330

    doc(rgb.rs): Add test for a module

--- src/styles/rgb.rs
+++ src/styles/rgb.rs
@@ -1,7 +1,19 @@
+/// A module for creating rgb color.
+///
+/// This module provides an struct `Rgb`
+/// that represents rgb color, with r,g,b field.
+/// It also implements the `Stylify` trait for `Rgb`,
+/// which allows for generating styles based on the paint type.
+
+// =======================================================================
+
 use super::{ paint_type::PaintType, Stylify };
 
 // =======================================================================
 
+/// An struct representing rgb color.
+///
+/// This struct has 3 field: `r`, `g` and `b` , which each of them represent 8 bit color code between 0 to 255.
 #[derive(Debug, Clone)]
 pub struct Rgb {
     pub r: u8,
@@ -10,6 +22,9 @@ pub struct Rgb {
 }
 
 impl Stylify for Rgb {
+    /// Returns a string representation of the rgb color code.
+    ///
+    /// If `paint_type` is `None`, the foreground color is assumed.
     fn make_styles(&self, paint_type: Option<&PaintType>) -> String {
         let paint_type = paint_type.unwrap_or(&PaintType::FG);
         format!(
