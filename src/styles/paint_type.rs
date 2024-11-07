217ce508770695c0f521cd7f2ebf7e3035c6eb0d
syaw0 <siamhb7@protonmail.com>
2024-11-07 10:24:48 +0330

    doc(paint_type.rs): Add document for a module

--- src/styles/paint_type.rs
+++ src/styles/paint_type.rs
@@ -1,14 +1,30 @@
+/// A module for working with paint types.
+///
+/// This module provides an enum `PaintType` that represents different types of paint.
+/// It also implements the `Stylify` trait for `PaintType`, which allows for generating styles based on the paint type.
+
+// =======================================================================
+
 use super::Stylify;
 
 // =======================================================================
 
+/// An enum representing different types of paint.
+///
+/// This enum has two variants: `FG` and `BG`, which represent foreground and background paint, respectively.
 #[derive(Debug, Clone)]
 pub enum PaintType {
+    /// Foreground paint.
     FG,
+    /// Background paint.
     BG,
 }
 
 impl Stylify for PaintType {
+    /// Generates styles based on the paint type.
+    ///
+    /// This method takes an optional `paint_type` parameter, which is ignored in this implementation.
+    /// It returns an empty string, indicating that no styles are generated.
     fn make_styles(&self, _paint_type: Option<&PaintType>) -> String {
         String::new()
     }
